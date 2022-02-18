5c6ef58b42be766f5a07f644b11a48e0786873bc
BrunoWM <bwillmoura3595@gmail.com>
2022-02-17 22:01:58 -0300

    first version

--- random_phrase/lib/main.dart
+++ random_phrase/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,17 +17,37 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _phrases = [
+    "phrase 0",
+    "phrase 1",
+    "phrase 2",
+    "phrase 3",
+    "phrase 4",
+  ];
+
+  String _initialPhrase = "your phrase will show up here";
+
+  void _generatePhrase() {
+    var randomNum = Random().nextInt(_phrases.length);
+    setState(() {
+      _initialPhrase = _phrases[randomNum];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Random Phrases"),
-        backgroundColor: Color.fromARGB(255, 255, 202, 40),
+        title: const Text("Random Phrases"),
+        backgroundColor: const Color.fromARGB(255, 255, 202, 40),
       ),
       body: Container(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(children: <Widget>[
-          Image.asset("logo.png"),
+          Image.asset("images/radom_logo.png"),
+          Text(_initialPhrase),
+          TextButton(
+              onPressed: _generatePhrase, child: const Text("nova frase"))
         ]),
       ),
     );
