25f248f132087ed48c432a33b01efb6ff63af185
BrunoWM <bwillmoura3595@gmail.com>
2022-03-18 23:27:14 -0300

    services

---
+++ mynotes/lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+// login exepitons
+class UserNotFoundAuthExcepion implements Exception {}
+
+class WrongPasswordAuthExcepion implements Exception {}
+
+// register exceptions
+class WeakPasswordAuthExcepion implements Exception {}
+
+class EmailAlredyInUseAuthExcepion implements Exception {}
+
+class InvalidEmailAuthExcepion implements Exception {}
+
+// generic exceptions
+class GenericAuthExcepion implements Exception {}
+
+class UserNotLoggedInAuthExcepion implements Exception {}
